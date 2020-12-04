8d988a9300d9cbd926e946d17e84e3377b0364d1
Danny Lin <danny@kdrag0n.dev>
2020-12-04 13:41:32 -0800

    boot-v1: test: Reboot to bootloader

--- boot-v1/test.sh
+++ boot-v1/test.sh
@@ -7,4 +7,5 @@ cd "$(dirname "$0")"
 cp ../rd-new.cpio ramdisk.gz
 cp ~/code/android/devices/zf6/proton/out/arch/arm64/boot/Image.gz-dtb zImage
 ./pack.sh
+adb reboot bootloader || true
 fastboot boot new.img
