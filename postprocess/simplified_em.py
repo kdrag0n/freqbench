a2ec410c13791e6c6f01da4d80834972f0b708d4
Danny Lin <danny@kdrag0n.dev>
2020-12-06 02:28:39 -0800

    postprocess: simplified_em: Add energy mode

--- postprocess/simplified_em.py
+++ postprocess/simplified_em.py
@@ -11,6 +11,7 @@ with open(sys.argv[1], "r") as f:
 
 cpus_data = json_data["cpus"]
 
+mode = "power"
 voltages = {}
 for arg in sys.argv[2:]:
     cluster, freq, voltage = map(int, re.split(r"\.|=", arg))
@@ -23,11 +24,16 @@ for cpu, cpu_data in cpus_data.items():
     for freq, freq_data in cpu_data["freqs"].items():
         freq = int(freq)
 
-        uw = freq_data["active"]["power_mean"] * 1000
+        if mode == "power":
+            # µW
+            cost = freq_data["active"]["power_mean"] * 1000
+        elif mode == "energy":
+            cost = freq_data["active"]["energy_millijoules"] * 10
+
         mhz = freq / 1000
         v = voltages[(cpu, freq)] / 1_000_000
 
-        dpc = uw / mhz / v**2
+        dpc = cost / mhz / v**2
         dpcs.append(dpc)
 
     print(f"cpu{cpu} = {statistics.mean(dpcs)}")
