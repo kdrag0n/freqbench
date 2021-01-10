4ccd120fb62177740304f3988db52a0369c94b95
Danny Lin <danny@kdrag0n.dev>
2021-01-10 01:38:00 -0800

    config: Remove obsolete block device option

--- config.sh
+++ config.sh
@@ -1,10 +1,5 @@
 # Common config options for freqbench
 
-# Should usually stay the same
-# Note that this is NOT like Android where everything is in /dev/block
-# For eMMC devices: /dev/mmcblk0
-BLOCK_DEV=/dev/sda
-
 # Whether to enable verbose debug logging during the benchmark
 # DO NOT ENABLE for final benchmarking!
 # The extra framebuffer memory copies caused by it will influence results.
